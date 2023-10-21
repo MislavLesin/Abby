af93f86b4cb8e7959b1f977d2d803762546e8725
MislavLesin <pripravnik-mislav.lesin@dump.hr>
2023-10-21 18:57:20 +0200

    Added client and server side validation

--- AbbyWeb/Pages/Categories/Create.cshtml.cs
+++ AbbyWeb/Pages/Categories/Create.cshtml.cs
@@ -5,10 +5,10 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AbbyWeb.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
-        [BindProperty]
         public Category Category { get; set; }
         public CreateModel(ApplicationDbContext db)
         {
@@ -20,9 +20,13 @@ namespace AbbyWeb.Pages.Categories
 
         public async Task<IActionResult> OnPost()
         {
-            await _db.Categories.AddAsync(Category);
-            await _db.SaveChangesAsync();
-            return RedirectToPage("Index");
+            if(ModelState.IsValid)
+            {
+                await _db.Categories.AddAsync(Category);
+                await _db.SaveChangesAsync();
+                return RedirectToPage("Index");
+            }
+            return Page();
         }
     }
 }
